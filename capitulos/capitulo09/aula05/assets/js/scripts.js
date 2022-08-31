02e01611d298df245735201e47d2c1e705cf1f21
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-08-31 08:17:35 -0300

    Toast e tooltip

--- capitulos/capitulo09/aula05/assets/js/scripts.js
+++ capitulos/capitulo09/aula05/assets/js/scripts.js
@@ -3,3 +3,9 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function(popoverTriggerEl){
     return new bootstrap.Popover(popoverTriggerEl);
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+
+var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl){
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+});
